ec9ce2878d95b1d0eb8711fde26ba1a5d5841c04
Tao Xu <xutaosjtu@gmail.com>
2012-10-23 17:44:36 +0200

    model selection and AUC estimation

--- CVD/Prospective analysis.R
+++ CVD/Prospective analysis.R
@@ -179,17 +179,182 @@ require(survival)
 rst = NULL;
 for (m in S4_valid_measures){
 	metabolite = S4[, m]
-	MI.cox = coxph(Surv(mi_time, S4$inz_mi) ~  log(metabolite) + ltalteru + log(ltsysmm) + log(ll_hdln) + log(ll_choln) + as.factor(lp_diab_who06) + as.factor(lcsex) , subset = which(S4$prev_mi == 0) , S4)
+	MI.cox = coxph(Surv(mi_time, inz_mi) ~  log(metabolite) +
+					ltalteru + as.factor(lcsex) + ltbmi## model 1
+					+(lp_diab_who06==4|lp_diab_who06==5)  ##model 2
+					+log(ll_choln)+log(ll_hdln)+log(ltsysmm)+ as.factor(ltcigreg)##model 3
+	 				+total2HDL ##model 4
+					+ltdiamm ## model 5
+					,subset = which(S4$prev_mi == 0) , S4)
 	rst = rbind(rst, summary(MI.cox)$coefficients[1,])
 }
 rst = data.frame(rst, FDR = p.adjust(rst[,5], method = "BH"), bonferroni = p.adjust(rst[,5], method = "bonferroni"))
 rownames(rst) = S4_valid_measures
-write.csv(rst, file = "MI survival analysis.csv")
+write.csv(rst, file = "MI survival analysis_model5.csv")
 
 plot(survfit(Surv(mi_time, S4$inz_mi)~(log(S4$PC_aa_C32_2) > 1.2), S4, subset= which(S4$prev_mi == 0)), log = "y", col = c("red","green"))
 
-tapply()
+###variable selection by boosting method
+metabolites.asso = scan(what = character())
+C14_1
+C16_1
+Arg
+Trp
+lysoPC_a_C16_0
+lysoPC_a_C16_1
+lysoPC_a_C17_0
+lysoPC_a_C18_0
+lysoPC_a_C18_1
+lysoPC_a_C18_2
+PC_aa_C28_1
+PC_aa_C32_2
+PC_aa_C34_2
+PC_aa_C34_3
+PC_aa_C34_4
+PC_aa_C36_2
+PC_aa_C36_3
+PC_aa_C36_6
+PC_ae_C36_2
+PC_ae_C38_0
+PC_ae_C38_2
+PC_ae_C40_1
+SM_C24_1
 
 
+require(CoxBoost)
+tmp = data.frame(
+		time = S4$mi_time, event = S4$inz_mi,  # time and events
+		S4$ltalteru, S4$ltbmi, as.factor(S4$lcsex), ##model1
+		(S4$lp_diab_who06==4|S4$lp_diab_who06==5), ##model 2
+		log(S4$ltsysmm),log(S4$ll_hdln), log(S4$ll_choln), as.factor(S4$ltcigreg),##model 3
+		log(S4$total2HDL), ##model 4
+		##log(S4$ltdiamm), ##model 5
+		log(as.matrix(S4[, metabo.selected]))
+)
+colnames(tmp)[3:11] = c("ltalteru", "ltbmi", "lcsex","lp_diab_who06", "ltsysmm", "ll_hdln", "ll_choln", "ltcigreg", "total2HDL")
+na.index = unique(unlist(apply(tmp, 2, function(x) which(is.na(x)))))
+subset = setdiff(which(S4$prev_mi == 0 & !is.na(S4$inz_mi)), na.index)
+# find optimal penalty
+penalty.optimal = optimCoxBoostPenalty(
+		time = S4$mi_time[subset], 
+		status = S4$inz_mi[subset],
+		x = as.matrix(tmp[subset, 12:31]),
+		minstepno = 1, 
+		maxstepno = 100)
+# find optimal boosting steps
+booststeps.optimal = cv.CoxBoost(
+		time = S4$mi_time[subset],
+		status = S4$inz_mi[subset], 
+		x = log(as.matrix(S4[subset, c(metabolites.asso)])),  
+		type="verweij")
+#variable selection
+MI.metabocox = CoxBoost(
+		time = S4$mi_time[subset ], 
+		status = S4$inz_mi[subset ], 
+		x =  as.matrix(tmp[subset, 12:31]),
+		stepno = 89, 
+		penalty = 603)
+
+metabo.selected = scan(what = character())
+Arg
+Trp
+lysoPC_a_C17_0
+lysoPC_a_C18_2
+PC_aa_C28_1
+PC_aa_C32_2
+PC_ae_C36_2
+PC_ae_C38_0
+PC_ae_C40_1
 
 
+#by boost in gbm which uses gradient descent method
+require(gbm) 
+MI.gbmboost = gbm(formula = y ~ .,
+		distribution = "coxph",
+		data = as.data.frame(tmp[subset,])
+		)
+data = data.frame( S4$mi_time, S4$inz_mi, tmp)
+
+#### random survival forest
+
+
+#### stepwise selection of cox regression
+selectCox <- function(formula, data, rule = "aic") {
+	require("rms")
+	require("prodlim")
+	fit <- cph(formula, data, surv = TRUE)
+	bwfit <- fastbw(fit, rule = rule)
+	if (length(bwfit$names.kept) == 0) {
+		newform <- reformulate("1", formula[[2]])
+		newfit <- prodlim(newform, data = data)
+	} else{
+		newform <- reformulate(bwfit$names.kept, formula[[2]])
+		newfit <- cph(newform, data, surv = TRUE)
+	}
+	out <- list(fit = newfit,In = bwfit$names.kept)
+	out$call <- match.call()
+	class(out) <- "selectCox"
+	out
+}
+
+selectCox(
+		formula = Surv(mi_time, inz_mi) ~  Arg+Trp+lysoPC_a_C16_0+lysoPC_a_C16_1+lysoPC_a_C17_0+lysoPC_a_C18_0+lysoPC_a_C18_1+lysoPC_a_C18_2+PC_aa_C28_1+PC_aa_C32_2+PC_aa_C34_2+PC_aa_C34_3+PC_aa_C34_4+PC_aa_C36_2+PC_aa_C36_3+PC_aa_C36_6+PC_ae_C36_2+PC_ae_C38_0+PC_ae_C40_1+SM_C24_1,
+		data = data.frame(log(S4[, metabolites.asso]), S4[, model1], mi_time = S4$mi_time, inz_mi = S4$inz_mi)[subset, ],
+		rule = "p")
+
++ ltalteru + log(ltdiamm) + log(ltsysmm) + log(ll_hdln) + log(ll_choln) + as.factor(lp_diab_who06) + as.factor(lcsex) + as.factor(ltcigreg)
+
+
+###################	evaluation by prediction error curve #############
+test = sample(subset, 300)
+train = setdiff(subset,test)
+
+train = subset
+
+Models <- list(
+		"Cox.metabolites" = coxph(Surv(time, event) ~ ., data = tmp[train, c(1:2, 12:20)]),
+		"Cox.clinical" = coxph(Surv(time, event) ~ ., data = tmp[train, c(1:11)]),
+		"Cox.all" = coxph(Surv(time, event) ~ ., data = tmp[train, ])
+)
+
+f = as.formula(paste("Surv(time, event)", paste(colnames(tmp)[-c(1:2)], collapse = "+"), sep = "~"))
+
+PredError <- pec::pec(object = Models, 
+		formula = f,
+		data = tmp[test,],
+		cens.model="marginal",
+		splitMethod = "none",
+		)
+plot(PredError, xlim = c(3000, 4000), smooth = T)		
+
+##survival ROC
+cutoff = max(tmp$time, na.rm = T)
+nobs = NROW(tmp)
+survival.all = survivalROC.C(
+		Stime = tmp$time[subset],
+		status = tmp$event[subset],
+		marker = tmp[subset, 12],
+		predict.time = cutoff,
+		span = 0.25*nobs^(-0.20)
+		)
+
+Surv.rsp <- Surv(tmp$time[train], tmp$event[train])
+Surv.rsp.new <- Surv(tmp$time[test], tmp$event[test])
+
+lpnew = predict(Models$Cox.all, newdata = tmp[test,-c(1:2)])
+lp = predict(Models$Cox.all)
+times = seq(2000,3700, 100)
+AUC_sh.all = AUC.sh(Surv.rsp,Surv.rsp.new,lp,lpnew,times)
+plot(AUC_sh.all,ylim = c(0.6, 0.8))
+
+lpnew = predict(Models$Cox.clinical, newdata = tmp[test,-c(1:2)])
+lp = predict(Models$Cox.clinical)
+times = seq(2000,3700, 100)
+AUC_sh.clinical = AUC.sh(Surv.rsp,Surv.rsp.new,lp,lpnew,times)
+plot(AUC_sh.clinical, add = T, col = "green")
+
+lpnew = predict(Models$Cox.metabolites, newdata = tmp[test,-c(1:2)])
+lp = predict(Models$Cox.metabolites)
+times = seq(2000,3700, 100)
+AUC_sh.metabolites = AUC.sh(Surv.rsp,Surv.rsp.new,lp,lpnew,times)
+plot(AUC_sh.clinical, add = T, col = "black")
