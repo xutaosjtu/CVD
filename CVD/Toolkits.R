342906d50c32e3e26d1c35e415042e2a4a6779ce
Tao Xu <xutaosjtu@gmail.com>
2012-10-26 09:29:17 +0200

    added cross validation function for cox regression

--- CVD/Toolkits.R
+++ CVD/Toolkits.R
@@ -128,3 +128,40 @@ Comparison.prospective<- function(baseline, feature, metabo, adj, subset){
 	
 }
 
+crossval.cox = function (x, y, theta.fit, theta.predict, ..., ngroup = n) 
+{
+	call <- match.call()
+	x <- as.matrix(x)
+	n <- length(y)/2
+	ngroup <- trunc(ngroup)
+	if (ngroup < 2) {
+		stop("ngroup should be greater than or equal to 2")
+	}
+	if (ngroup > n) {
+		stop("ngroup should be less than or equal to the number of observations")
+	}
+	if (ngroup == n) {
+		groups <- 1:n
+		leave.out <- 1
+	}
+	if (ngroup < n) {
+		leave.out <- trunc(n/ngroup)
+		o <- sample(1:n)
+		groups <- vector("list", ngroup)
+		for (j in 1:(ngroup - 1)) {
+			jj <- (1 + (j - 1) * leave.out)
+			groups[[j]] <- (o[jj:(jj + leave.out - 1)])
+		}
+		groups[[ngroup]] <- o[(1 + (ngroup - 1) * leave.out):n]
+	}
+	u <- NULL
+	cv.fit <- rep(NA, n)
+	for (j in 1:ngroup) {
+		u <- theta.fit(x[-groups[[j]], ], y[-groups[[j]]])
+		cv.fit[groups[[j]]] <- theta.predict(u, x[groups[[j]],])
+	}
+	if (leave.out == 1) 
+		groups <- NULL
+	return(list(cv.fit = cv.fit, ngroup = ngroup, leave.out = leave.out, 
+					groups = groups, call = call))
+}
